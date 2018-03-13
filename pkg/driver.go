0b3519a79c444597e5aa0b2430b8e4672cca43b6
SKD-SKD <34695066+SKD-SKD@users.noreply.github.com>
2018-03-13 15:40:53 -0400

    Update driver.go

--- pkg/driver.go
+++ pkg/driver.go
@@ -1,1 +1,91 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package testD
+
+import (
+	"github.com/golang/glog"
+
+	"github.com/container-storage-interface/spec/lib/go/csi/v0"
+	"github.com/kubernetes-csi/drivers/pkg/csi-common"
+)
+
+type testD struct {
+	driver *csicommon.CSIDriver
+
+	ids *identityServer
+	ns  *nodeServer
+	cs  *controllerServer
+
+	cap   []*csi.VolumeCapability_AccessMode
+	cscap []*csi.ControllerServiceCapability
+}
+
+var (
+	testDDiver *testD
+	version    = "0.2.0"
+)
+
+func GettestDDriver() *testD {
+	return &testD{}
+}
+
+func NewIdentityServer(d *csicommon.CSIDriver) *identityServer {
+	return &identityServer{
+		DefaultIdentityServer: csicommon.NewDefaultIdentityServer(d),
+	}
+}
+
+func NewControllerServer(d *csicommon.CSIDriver) *controllerServer {
+	return &controllerServer{
+		DefaultControllerServer: csicommon.NewDefaultControllerServer(d),
+	}
+}
+
+func NewNodeServer(d *csicommon.CSIDriver) *nodeServer {
+	return &nodeServer{
+		DefaultNodeServer: csicommon.NewDefaultNodeServer(d),
+	}
+}
+
+func (testD *testD) Run(driverName, nodeID, endpoint string) {
+	glog.Infof("Driver: %v version: %v", driverName, version)
+
+	// Initialize default library driver
+	testD.driver = csicommon.NewCSIDriver(driverName, version, nodeID)
+	if testD.driver == nil {
+		glog.Fatalln("Failed to initialize CSI Driver.")
+	}
+  
+	testD.driver.AddControllerServiceCapabilities([]csi.ControllerServiceCapability_RPC_Type{
+		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
+		csi.ControllerServiceCapability_RPC_PUBLISH_UNPUBLISH_VOLUME,
+	}
+	testD.driver.AddVolumeCapabilityAccessModes([]csi.VolumeCapability_AccessMode_Mode{csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER})
+
+	// Create GRPC servers
+	testD.ids = NewIdentityServer(testD.driver)
+	testD.ns = NewNodeServer(testD.driver)
+	testD.cs = NewControllerServer(testD.driver)  
+	s := csicommon.NewNonBlockingGRPCServer()
+  
+	s.Start(endpoint, testD.ids, testD.cs, testD.ns)
+  
+	s.Wait()
+}
+
+
 
