d2500b607604a8f773ef384e4b57de70439fe2fe
SKD-SKD <34695066+SKD-SKD@users.noreply.github.com>
2018-03-14 17:30:32 -0400

    Update driver.go

--- pkg/tstD/driver.go
+++ pkg/tstD/driver.go
@@ -69,11 +69,11 @@ func (testD *testD) Run(driverName, nodeID, endpoint string) {
 	if testD.driver == nil {
 		glog.Fatalln("Failed to initialize CSI Driver.")
 	}
-        
+	
 	//Add Capability
 	testD.driver.AddControllerServiceCapabilities([]csi.ControllerServiceCapability_RPC_Type{csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME})
 	testD.driver.AddVolumeCapabilityAccessModes([]csi.VolumeCapability_AccessMode_Mode{csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER})
-
+	
 	// Create GRPC servers 
 	testD.ids = NewIdentityServer(testD.driver)
 	testD.ns = NewNodeServer(testD.driver)
